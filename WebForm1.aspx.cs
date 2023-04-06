e1ba283eaae51509a587f40e2ed1d6d487d21e44
Máximo Canedo <30154899delacroix@gmail.com>
2023-04-05 21:39:10 -0300

    Optimizaciones varias

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,34 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
-namespace TrabajoPractico2
-{
-    public partial class WebForm1 : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+namespace TrabajoPractico2 {
+    public partial class WebForm1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
 
         }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
+        /// <summary>
+        /// Genera una tabla HTML a partir de los valores ingresados en los cuadros de texto.
+        /// </summary>
+        protected void generarTabla() {
+            // Obtener los valores de los cuadros de texto.
             string primerProducto = TextBox1.Text;
             string segundoProducto = TextBox3.Text;
-            int cantProducto1 = int.Parse(TextBox2.Text);
-            int cantProduct2 = int.Parse(TextBox4.Text);
 
-            string table = "<table border='1'><tr><td>Producto</td><td>Cantidad</td></tr>";
-            table += "<tr><td>" + primerProducto + "</td><td>" + cantProducto1 + "</td></tr>";
-            table += "<tr><td>" + segundoProducto + "</td><td>" + cantProduct2 + "</td></tr>";
-            table += "<tr><td>Total</td><td>" + (cantProducto1 + cantProduct2) + "</td></tr>";
-            table += "</table>";
-            Tabla.Text = table;
+            int cantProducto1;
+            int cantProducto2;
+            if (!int.TryParse(TextBox2.Text, out cantProducto1) || !int.TryParse(TextBox4.Text, out cantProducto2)) {
+                // El valor del cuadro de texto no se puede convertir a un número entero.
+                Element.Alert("El valor ingresado no se puede convertir a un número entero. ", this);
+                return;
+            }
 
+            // Calcular la cantidad total de productos.
+            int cantidadTotal = cantProducto1 + cantProducto2;
 
+            // Crea una nueva tabla HTML.
+            Element.Table table = new Element.Table();
+
+            // Establece los encabezados de la tabla.
+            table.SetHeaders("Producto", "Cantidad");
+
+            // Establece las filas de la tabla.
+            table.AddRow(primerProducto, cantProducto1);
+            table.AddRow(segundoProducto, cantProducto2);
+            table.AddRow("Total", cantidadTotal);
+
+            // Muestra la tabla en el control Tabla.
+            Tabla.Text = table.ToString();
+        }
+        protected void Button1_Click(object sender, EventArgs e) {
+            generarTabla();
         }
     }
 }
